27d3004b32e6d8f13ed24f900a7ac06b5047b133
ran.wahle@gmail.com <ran.wahle@gmail.com>
2015-08-11 14:02:55 +0300

    Create agent with angular client

---
+++ TheAgency.Web/app/services/agentsService.js
@@ -0,0 +1,29 @@
+﻿(function (angular) {
+
+    var service  = function($http)
+    {
+        this.getAgents = function () {
+            var promise = $http({
+                url: 'api/Agents',
+                method: 'get'
+            });
+
+            return promise;
+        };
+
+        this.createAgent = function (agent) {
+            var promise = $http({
+                url: 'api/Agents',
+                method: 'post',
+                data: agent
+
+            });
+
+            return promise;
+        };
+
+       
+    }
+    angular.module('agents').service('agentsService', ['$http', service]);
+
+}(window.angular));
